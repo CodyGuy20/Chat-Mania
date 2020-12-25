c9ff439c6132aaf48dd1225b0564d0d9c4e172e5
Hemang <hemang@Apples-MacBook-Air-4.local>
2020-12-25 10:36:58 +0530

    changes made to the project

--- public/js/main.js
+++ public/js/main.js
@@ -3,27 +3,30 @@ const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
+// Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
     ignoreQueryPrefix: true
 });
+
 const socket = io();
 
+// Join chatroom
 socket.emit('joinRoom', { username, room });
 
+// Get room and users
 socket.on('roomUsers', ({ room, users }) => {
     outputRoomName(room);
     outputUsers(users);
 });
 
-
+// Message from server
 socket.on('message', message => {
     console.log(message);
     outputMessage(message);
 
+    // Scroll down
     chatMessages.scrollTop = chatMessages.scrollHeight;
 });
-
-
 
 // Message submit
 chatForm.addEventListener('submit', e => {
@@ -46,11 +49,12 @@ chatForm.addEventListener('submit', e => {
     e.target.elements.msg.focus();
 });
 
+// Output message to DOM
 function outputMessage(message) {
     const div = document.createElement('div');
     div.classList.add('message');
     const p = document.createElement('p');
-    p.classList.add('meta')
+    p.classList.add('meta');
     p.innerText = message.username;
     p.innerHTML += `<span>${message.time}</span>`;
     div.appendChild(p);
